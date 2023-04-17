a3c9941d4e6b34ca9b7ee85538e8626162d7d484
AleksiDu <aleksi.duluzauri@gmail.com>
2023-04-17 13:57:59 +0400

    setup appolographql

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import microConfig from "./mikro-orm.config";
 import { Post } from "./entities/Post";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { resolve } from "path";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -11,9 +14,11 @@ const main = async () => {
 
   const app = express();
 
-  app.get("/", (_, res) => {
-    res.send("Hello World!");
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [HelloResolver], validate: false }),
   });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
     console.log("server started on localhost:4000");
