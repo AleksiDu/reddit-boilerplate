d1f91dfa608ef24ebaeea0a107208fc42e9753ca
AleksiDu <aleksi.duluzauri@gmail.com>
2023-04-17 12:39:44 +0400

    initial migration

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,19 @@
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import path from "path";
 
 export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    glob: "!(*.d).{js,ts}", // how to match migration files (all .js and .ts files, but not .d.ts)
+  },
   entities: [Post],
   dbName: "lireddit",
   type: "postgresql",
   debug: !__prod__,
-  user: "",
-  password: "",
+  user: "postgres",
+  password: "aa123456",
   host: "localhost",
   port: 5432,
 } as Parameters<typeof MikroORM.init>[0];
