0689ef5c22e102dc42b38ba91d3fd261d21745b3
AleksiDu <aleksi.duluzauri@gmail.com>
2023-04-17 13:30:48 +0400

    setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
 import { Post } from "./entities/Post";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   const em = orm.em.fork(); // Create a new EntityManager instance
-  // const post = em.create(Post, {
-  //   title: "my first post",
-  //   createdAt: new Date(),
-  //   updatedAt: new Date(),
-  // });
-  // await em.persistAndFlush(post);
-  // const posts = await em.find(Post, {});
-  // console.log(posts);
+
+  const app = express();
+
+  app.get("/", (_, res) => {
+    res.send("Hello World!");
+  });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => console.log(err));
