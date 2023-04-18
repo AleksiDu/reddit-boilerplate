fe20b7f883f77d413c373d37593831288e0f16ac
AleksiDu <aleksi.duluzauri@gmail.com>
2023-04-18 20:38:43 +0400

    Add PostResolver with posts query

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import microConfig from "./mikro-orm.config";
-import { Post } from "./entities/Post";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import { resolve } from "path";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -15,7 +15,11 @@ const main = async () => {
   const app = express();
 
   const apolloServer = new ApolloServer({
-    schema: await buildSchema({ resolvers: [HelloResolver], validate: false }),
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: em }),
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
